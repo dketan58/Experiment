7dc7978b548d09a8683a9c7e1627376dc843025b
dketan58 <d.ketan58@gmail.com>
2019-12-12 15:07:55 +0530

    Reversing the string

---
+++ indexing.py
@@ -0,0 +1,11 @@
+"""
+Purpose: Python List Indexing
+Author:Ketan Dekate
+Date: 12 Dec 2019
+"""
+employee_ids=[1,2,3,4,5,6,7,8]
+print("The existing string is: ")
+print(employee_ids)
+employee_ids.reverse()
+print("The string after reversing is: ")
+print(employee_ids)
