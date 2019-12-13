e806a2daf2b692328131366a6788ddff64dc766d
dketan58 <d.ketan58@gmail.com>
2019-12-13 10:59:42 +0530

    dcba

--- dummy.py
+++ dummy.py
@@ -3,7 +3,7 @@ Purpose: Python List Indexing
 Author:Ketan Dekate
 Date: 12 Dec 2019
 """
-employee_ids=[11,12,13,14]
+employee_ids=[14,13,12,11]
 print("The existing string is: ")
 print(employee_ids)
 employee_ids.reverse()
